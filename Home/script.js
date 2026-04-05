979d1b210926645b2b3788259e6b11a345e8f6da
Cesar Augusto <cesarprofissional22@gmail.com>
2026-04-05 20:12:32 -0300

    feat: Criando logica js, direcionando colaborador pro formulario

---
+++ Home/script.js
@@ -0,0 +1,5 @@
+const btnDocumentacao = document.getElementById("btnDocumentacao");
+
+btnDocumentacao.addEventListener("click", function () {
+    window.location.href = "https://docs.google.com/forms/d/e/1FAIpQLSfiAQgsI-pzqheL__LOKvw9bx5hsaVT8fgvL3yj2K7AYbO2nQ/viewform?usp=dialog";
+});
